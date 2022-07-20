7bd4a4cad8e592e257c0b9d8f7476c0b2f191082
Andrew Gavegan <zzandrewzz@optusnet.com.au>
2022-07-20 13:46:44 +1000

    finished mutations now that input type can be used for savebooks

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -18,9 +18,45 @@ export const ADD_USER = gql`
     addUser(username: $username, email: $email, password: $password) {
       token
       user {
-        _id
+        userID
         username
+        email
+
       }
     }
   }
 `;
+
+export const SAVE_BOOK = gql`
+mutation saveBook($input: BookInfo!) {
+  saveBook(input: $input) {
+    userID
+    username
+    email
+    savedBooks {
+      bookID
+      authors
+      description 
+      title 
+      image 
+      link 
+    }
+  }
+}`;
+
+export const REMOVE_BOOK = gql`
+  mutation removeBook($bookID: String!) {
+    removeBook(bookID: $bookID) {
+      userID
+      username
+      email
+      savedBooks {
+      bookID
+      authors
+      description 
+      title 
+      image 
+      link 
+      }
+    }
+  }`;
