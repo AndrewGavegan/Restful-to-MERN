7bdeda64c086d8f5d3df85c1bd7063786e790802
Andrew Gavegan <zzandrewzz@optusnet.com.au>
2022-07-21 15:22:35 +1000

    cant deploy

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/cluster0', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/restful-to-mern', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
